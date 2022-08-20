6529081f02e9ed98fb328db6f9f32803499c9409
Shujaatahmed786 <95533854+Shujaatahmed786@users.noreply.github.com>
2022-08-20 23:21:05 +0500

    Add files via upload

---
+++ test_prime.js
@@ -0,0 +1,17 @@
+function test_prime() {
+
+    if (n === 1) {
+        return false;
+    }
+    else if (n === 2) {
+        return true;
+    }
+    else {
+        for (var x = 2; x < n; x++) {
+            if (n % x === 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
